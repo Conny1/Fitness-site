fb5533914b62cb5c99588730a9d0c97c2a74d673
Conny1 <joelconrad277@gmail.com>
2023-03-22 11:51:27 +0300

    'changes'

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Home.css'
 import {BodyMass, Featured, Footer} from '../../componets'
+import gym from  '../../utils/img/gym.jfif'
 
 const Home = () => {
   return (
     <div className='home' >
      <div className='header' >
-     <img src="https://www.mensjournal.com/wp-content/uploads/2018/06/man-weight-lifting.jpg?w=800&h=450&crop=1&quality=86&strip=all" alt="Man lifting weights" />
+     <img src={gym} alt="Man lifting weights" />
     
      <div className='headerInfo' >
       <p  className='a' >Grow your</p>
